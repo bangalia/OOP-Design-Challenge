d8817e034320c6870dc5b48e0f8161ce8ac39b2d
lotsofluck <anitabangali@yahoo.com>
2020-12-07 16:23:29 -0600

    Update bakery.py

---
+++ bakery.py
@@ -0,0 +1,7 @@
+class Bakery:
+    def __init__(self,greeting):
+        self.greeting = greeting
+
+    def welcome(self):
+        greeting = "Hello and welcome to the Bakery!"
+        print(greeting)
