b3805c74a7c66e2e636a8c74d44f644981719c33
lotsofluck <anitabangali@yahoo.com>
2020-12-08 12:52:19 -0600

    Created order class

--- bakery.py
+++ bakery.py
@@ -1,6 +1,7 @@
 class Bakery:
     def __init__(self,greeting):
         self.greeting = greeting
+        #self.goodbye = goodbye
 
     def welcome(self):
         greeting = "Hello and welcome to the Bakery!"
---
+++ order.py
@@ -0,0 +1,6 @@
+import random
+
+class Order:
+    def __init(self,order_number,order_contents):
+        self.order_number = order_number
+        self.order_contents = order_contents
